e324652ee3ec67e015bce946815f0a54d5ae34d9
lina-shakhshir <sh3719143@gmail.com>
2026-03-06 16:11:54 +0200

    added HomeController, published web and DB

--- MVCProject/Data/ApplicationDbContext.cs
+++ MVCProject/Data/ApplicationDbContext.cs
@@ -6,8 +6,14 @@ namespace MVCProject.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+
+
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=.;Database=MVCProject;TrustServerCertificate=True;Trusted_Connection=True");
+            //DB for production (publish)
+            optionsBuilder.UseSqlServer("Server=db43627.public.databaseasp.net; Database=db43627; User Id=db43627; Password=d@9J!Cg52+Yj; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True; ");
+
+            //DB for dev
+           // optionsBuilder.UseSqlServer("Server=.;Database=MVCProject;TrustServerCertificate=True;Trusted_Connection=True");
         }
 
 
