79566ab7f60fc25c6eb8856b9068c6616325e15a
lina-shakhshir <sh3719143@gmail.com>
2026-05-01 00:18:48 +0300

    refactor database connection setup

--- MVCProject/Data/ApplicationDbContext.cs
+++ MVCProject/Data/ApplicationDbContext.cs
@@ -13,12 +13,13 @@ namespace MVCProject.Data
 
             base.OnConfiguring(optionsBuilder);
             //DB for production (publish)
-          optionsBuilder.UseSqlServer("Server=db43627.public.databaseasp.net; Database=db43627; User Id=db43627; Password=d@9J!Cg52+Yj; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True; ");
+            optionsBuilder.UseSqlServer("REPLACE_WITH_YOUR_CONNECTION_STRING");
 
             //DB for dev
-          // optionsBuilder.UseSqlServer("Server=.;Database=MVCProject;TrustServerCertificate=True;Trusted_Connection=True");
+            // optionsBuilder.UseSqlServer("Server=.;Database=MVCProject;TrustServerCertificate=True;Trusted_Connection=True");
         }
 
 
     }
+    
 }
